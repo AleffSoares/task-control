39ef3be6f72c12204fed7ac6c66f756a1504370d
Aleff Soares <aleffsoares8@gmail.com>
2023-02-22 21:35:00 -0400

    Criado o input, botão e estilização

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,12 @@
-import { Text, View } from "react-native";
+import React, { useRef, useState} from 'react';
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 import { styles } from "./styles";
 
 
 export default function Home () {
+  const inputRef = useRef(null);
+  const [isFocused, setIsFocused] = useState(false);
   return (
       <View style={styles.container}>
         <View style={styles.title}>
@@ -13,7 +16,25 @@ export default function Home () {
           <Text  style={styles.control}>
                 Control
           </Text>
-        </View> 
+        </View>
+
+        <View style={styles.form}>
+            <TextInput
+              ref={inputRef}
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)} 
+              style={isFocused ? styles.inputFocus : styles.input}
+              placeholder="Adicione uma nova tarefa"
+              placeholderTextColor='#454545'
+            />
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>
+                +
+              </Text>
+            </TouchableOpacity>
+        </View>
+
+        
       </View>
   );
 }
